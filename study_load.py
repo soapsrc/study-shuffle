cd67c67746b834d22cba03d2184d9fe2e702bea5
Sophia L <sophitole@gmail.com>
2021-11-09 12:28:34 -0700

    Load urls in separate python file

---
+++ study_load.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service
+import time
+
+# A class to store all the details per Category
+class Category:
+  def __init__(self , name , urls):
+    self.name  = 	name  
+    self.urls = 	urls
+
+def driverSetup():
+        # Hides the browser window containing the search results
+    co = webdriver.ChromeOptions()
+    co.add_argument('headless')
+    co.add_argument('window-size=1920x1080')
+    co.add_argument("disable-gpu")
+    # Path to webdriver
+    serv = Service('./chromedriver')
+    browser = webdriver.Chrome(service=serv, options=co)
+    return browser
+
+def loadUrls(browser):
+    queries = ['anime', 'beach', 'window', 'r&b']
+    hrefs = []
+    categories = []
+
+    # For each category, save the the relevant YouTube video urls to the appropriate array
+    for i in range(0, len(queries)):
+        print("Storing all " + queries[i] + " urls")
+        url = 'https://www.youtube.com/results?search_query=' + queries[i] + '+study+ambience'
+        browser.get(url)
+        # Parse html of webpage
+        source = browser.page_source
+        soup = BeautifulSoup(source,'html.parser')
+        browser.get(url)
+        # Find the YouTube urls of all the videos that resulted from each query
+        hrefs = soup.find_all("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")
+        ytURLS = []
+        for a in hrefs:
+            ytURLS.append(a['href'])
+        cat = Category(queries[i], ytURLS)
+        categories.append(cat)
+        print(categories[i].urls)
+        # Sleep for 2 seconds to make sure webpage has been thoroughly parsed
+        time.sleep(2)
--- study_shuffle.py
+++ study_shuffle.py
@@ -1,43 +1,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 import random
-import time
+import study_load
 
-# Hides the browser window containing the search results
-co = webdriver.ChromeOptions()
-co.add_argument('headless')
-co.add_argument('window-size=1920x1080')
-co.add_argument("disable-gpu")
-# Path to webdriver
-serv = Service('./chromedriver')
-browser = webdriver.Chrome(service=serv, options=co)
-
-queries = ['anime', 'beach', 'window', 'r&b']
-hrefs = []
-category1 = []
-category2 = []
-category3 = []
-category4 = []
-allUrls = [category1, category2, category3, category4]
-
-# For each category, save the the relevant YouTube video urls to the appropriate array
-for i in range(0, len(queries)):
-    print("Storing all " + queries[i] + " urls")
-    url = 'https://www.youtube.com/results?search_query=' + queries[i] + '+study+ambience'
-    browser.get(url)
-    # Parse html of webpage
-    source = browser.page_source
-    soup = BeautifulSoup(source,'html.parser')
-    browser.get(url)
-    # Find the YouTube urls of all the videos that resulted from each query
-    hrefs = soup.find_all("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")
-    for a in hrefs:
-        allUrls[i].append(a['href'])
-    print(allUrls[i])
-    # Sleep for 2 seconds to make sure webpage has been thoroughly parsed
-    time.sleep(2)
+browser = study_load.driverSetup()
+categories = study_load.loadUrls(browser)
 
 # Pick a category
 num = int(input("1 - Anime\n2 - Beach\n3 - Window\n4 - R&B\nEnter the number associated to the video category you want to study to\n"))
@@ -52,9 +20,9 @@ else:           # R&B
 
 categoryNum = num -1
 # Select a random video
-rand = random.randint(0,len(allUrls[categoryNum]))
+rand = random.randint(0,len(categories[categoryNum].urls))
 # Parse for YouTube video ID
-videoID = allUrls[categoryNum][rand].split('=')
+videoID = categories[categoryNum].urls[rand].split('=')
 # Open video in browser and play it
 print("Playing " + query + " video #" + str(rand))
 browser = webdriver.Chrome(service=serv)
